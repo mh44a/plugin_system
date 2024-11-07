59298555fdbf90795c28788987ac73ec0788d341
Poyraz Menlik <menlikpoyraz@gmail.com>
2024-11-07 12:51:49 +0300

    test

--- crypter.js
+++ crypter.js
@@ -1,15 +1,13 @@
-const crypto = require('crypto');
 const fs = require('fs');
 const Cryptify = require('cryptify');
 
-const algorithm = 'aes-256-ctr';
 const password = 'e7G8NcL7-b9a1-4J9S-8C5C-fP29L10GbbN3';
 
 const instance = new Cryptify('testPlugin.js', password);
 instance
   .encrypt()
   .then((files) => {
-    fs.writeFileSync('testPlugin_encoded.js', files[0]);
+    fs.writeFileSync('testPlugin_encoded.js', JSON.stringify(files[0]));
     console.log('Encryption successful');
   })
   .then(() => instance.decrypt())

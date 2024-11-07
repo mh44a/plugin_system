de1e8aa15dbc900802b71670bb64dfc3d6e17445
Poyraz Menlik <menlikpoyraz@gmail.com>
2024-11-07 13:59:14 +0300

    sidebar

--- testPlugin.js
+++ testPlugin.js
@@ -1,30 +1,9 @@
-const generateRandomString = (length) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
+function addData(array, data) {
+    array.push(data);
 }
 
 export default {
     install(app) {
-        app.config.globalProperties.$multiply = (a, b) => a * b;
-        app.config.globalProperties.$generateRandomString = (length) => {
-            return generateRandomString(length);
-        }
-
-        app.config.globalProperties.$pluginSendRequest = async (data) => {
-            try {
-                const response = await app.config.globalProperties.$sendAPIRequest(
-                    'https://httpbin.org/post',
-                    data
-                );
-                console.log("API response:", response);
-                return response;
-            } catch (error) {
-                console.error("API request failed in plugin:", error);
-            }
-        };
+        app.config.globalProperties.$addSidebarItem = (siderBarArray, route, name, icon) => addData(array, { route, name, icon });
     },
 };

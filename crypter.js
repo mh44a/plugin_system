64e45b8d9574b9271d8c187c2945c46c254f5fac
Poyraz Menlik <menlikpoyraz@gmail.com>
2024-11-07 12:17:38 +0300

    sa

--- crypter.js
+++ crypter.js
@@ -6,13 +6,13 @@ const password = 'e7G8NcL7-b9a1-4J9S-8C5C-fP29L10GbbN3';
 
 const encrypt = (data) => {
   const cipher = crypto.createCipher(algorithm, password);
-  let encrypted = cipher.update(data, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
+  let encrypted = cipher.update(data, 'utf8', 'base64');
+  encrypted += cipher.final('base64');
   return encrypted;
 };
 
 const pluginData = fs.readFileSync('testPlugin.js', 'utf8');
 const encryptedData = encrypt(pluginData);
 
+// Şifreli veriyi base64 formatında kaydediyoruz
 fs.writeFileSync('encryptedPlugin.js', encryptedData);
-console.log("Plugin şifrelendi.");

287512d76787a440a65f2db015aa0f3f019d864c
Poyraz Menlik <menlikpoyraz@gmail.com>
2024-11-07 11:32:55 +0300

    gg

--- testPlugin.js
+++ testPlugin.js
@@ -1,10 +1,19 @@
 export default {
     install(app) {
-        app.config.globalProperties.$multiply = (a, b) => a * b;
-        app.config.globalProperties.$test = "Test String";
-
-        app.config.globalProperties.$getArray = () => {
-            return ["Günili", "GoldenMES"];
-        };
-    }
-};
+      app.config.globalProperties.$multiply = (a, b) => a * b;
+  
+      app.config.globalProperties.$pluginSendRequest = async (data) => {
+        try {
+          const response = await app.config.globalProperties.$sendAPIRequest(
+            'https://httpbin.org/post',
+            data
+          );
+          console.log("API response:", response);
+          return response;
+        } catch (error) {
+          console.error("API request failed in plugin:", error);
+        }
+      };
+    },
+  };
+  
